0fc3525d24d3af174485ab91aa503e4bae270db6
Klimenko.E <klimenko.e@bk.ru>
2023-07-11 19:17:44 +0300

    Add CsvFile

--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -1,31 +1,19 @@
 package ru.netology.services;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 //import ru.netology.services.VacationService;
 
 public class VacationServiceTest {
 
-    @Test
-    public void vacatin10_3_20() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/vacation.csv")
+    public void vacation(int income, int expense, int threshold, int expected) {
 
         VacationService service = new VacationService();
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
+        int actual = service.calculate(income, expense, threshold);
 
         Assertions.assertEquals(expected, actual);
-
     }
-
-    @Test
-    public void vacatin100_60_150() {
-
-        VacationService service = new VacationService();
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
-
-        Assertions.assertEquals(expected, actual);
-
-    }
-
 }
